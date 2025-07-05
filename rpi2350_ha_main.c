3cb5a993c3020083e0f3b1206c1c3611c7f280e7
pkarthikbit <pkarthikbit@gmail.com>
2025-07-05 20:41:16 +0530

    usb output instead of uart

--- rpi2350_ha_main.c
+++ rpi2350_ha_main.c
@@ -1,8 +1,17 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 
 int main() {
     stdio_init_all();
-    printf("Hello, world!\n");
-    return 0;
+    while (true) 
+    {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
 }
