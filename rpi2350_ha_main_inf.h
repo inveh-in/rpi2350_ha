f84dbec01490ffb8e3bcbd740ffd9ac84b1d2732
pkarthikbit <pkarthikbit@gmail.com>
2025-07-06 20:09:32 +0530

    inclusion for freeRtos

--- rpi2350_ha_main_inf.h
+++ rpi2350_ha_main_inf.h
@@ -8,8 +8,7 @@
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"    // Pico W devices use a GPIO on the WIFI chip for the LED
-#include "pico/async_context_freertos.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
-#include "FreeRTOSConfig.h
+
