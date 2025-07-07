b9916d2b00b79139c2b596d04066460fdce8ca45
pkarthikbit <pkarthikbit@gmail.com>
2025-07-08 05:03:24 +0530

    rtos config files moved inside a folder

--- kpl_build.sh
+++ kpl_build.sh
@@ -1,7 +1,3 @@
-export PICO_SDK_PATH=/home/priyanka/pico-sdk
-export PICOTOOL_FETCH_FROM_GIT_PATH=/home/priyanka/picotool
-export FREERTOS_KERNEL_PATH=/home/priyanka/FreeRTOS-Kernel
-
 mkdir build
 cd build
 cmake -DPICO_BOARD=pico2_w ..
