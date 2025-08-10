59972960e0e8404a2c6cccc1fe55dd84d5a7ee80
pkarthikbit <pkarthikbit@gmail.com>
2025-08-10 17:44:04 +0530

    pause before monitoring

--- kpl_flash.sh
+++ kpl_flash.sh
@@ -1,2 +1,3 @@
 sudo /home/priyanka/.pico-sdk/picotool/2.1.1/picotool/picotool load /home/priyanka/Documents/rpi2350_ha/build/rpi2350_ha.elf -fx
+sleep 4 # Pause the script
 plink -serial /dev/ttyACM0 -sercfg 11520,8,n,1
