bb6d487644c335360de3a6576900bd392705edbd
pkarthikbit <pkarthikbit@gmail.com>
2025-08-10 21:34:29 +0530

    now ble works

--- rpi2350_ha_pub.h
+++ rpi2350_ha_pub.h
@@ -1,9 +1,7 @@
 #ifndef _RPI2350_HA_PUB_H
 #define _RPI2350_HA_PUB_H
 
-extern void rpi2350_ha_ble_init(void);
 extern void rpi2350_ha_ble_proc(__unused void *params);
-extern void rpi2350_ha_wifi_init(void);
 extern void rpi2350_ha_wifi_proc(__unused void *params);
 
 #endif /* _RPI2350_HA_PUB_H */
